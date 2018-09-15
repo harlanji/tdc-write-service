903ab44037183c1608d60a1286167bc5f95841cd
Harlan Iverson <h.iverson@gmail.com>
2018-09-14 17:37:44 -0700

    refactored support-rules out of main rules handler

--- src/tdc_write_service/rules.clj
+++ src/tdc_write_service/rules.clj
@@ -1,32 +1,15 @@
 (ns tdc-write-service.rules
   (:require [ring.util.response :as ring-res]
+            [tdc-write-service.support-rules :as support-rules]
             [clara.rules :as clara]))
-
-(defrecord SupportRequest [client level])
-
-(defrecord ClientRepresentative [name client])
-
-(clara/defrule is-important
-  "Find important support requests."
-  [SupportRequest (= :high level)]
-  =>
-  (println "High support requested!"))
-
-(clara/defrule notify-client-rep
-  "Find the client representative and request support."
-  [SupportRequest (= ?client client)]
-  [ClientRepresentative (= ?client client) (= ?name name)]
-  =>
-  (println "Notify" ?name "that"
-           ?client "has a new support request!"))
 
 (defn run-rules-handler [request]
   (->
-   (clara/mk-session 'tdc-write-service.core)
-   (clara/insert (->ClientRepresentative "Alice" "Acme")
-                 (->SupportRequest "Acme" :high))
+   (clara/mk-session 'tdc-write-service.support-rules)
+   (clara/insert (support-rules/->ClientRepresentative "Alice" "Acme")
+                 (support-rules/->SupportRequest "Acme" :high))
    (clara/fire-rules))
-  (ring-res/response "Ran the ruules"))
+  (ring-res/response "Ran the support ruules"))
 
 ; -- commands + model
 
---
+++ src/tdc_write_service/support_rules.clj
@@ -0,0 +1,21 @@
+(ns tdc-write-service.support-rules
+  (:require [clara.rules :as clara]))
+
+(defrecord SupportRequest [client level])
+
+(defrecord ClientRepresentative [name client])
+
+(clara/defrule is-important
+  "Find important support requests."
+  [SupportRequest (= :high level)]
+  =>
+  (println "High support requested!"))
+
+(clara/defrule notify-client-rep
+  "Find the client representative and request support."
+  [SupportRequest (= ?client client)]
+  [ClientRepresentative (= ?client client) (= ?name name)]
+  =>
+  (tdc-write-service.upload/path "")
+  (println "Notify" ?name "that"
+           ?client "has a new support request!"))
